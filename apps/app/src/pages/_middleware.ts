ab0b7e5d69fbd54c85ee5b253e6aae9043910a1d
Hendrik Schuster <h.schuster@deviantdev.com>
2021-12-23 00:28:38 +0100

    Revert "TEST Remove middleware"
    This reverts commit bab7c5ecd3e1a8fd5201d21ac8bac4f2d154ebe8.

---
+++ apps/app/src/pages/_middleware.ts
@@ -0,0 +1,82 @@
+import type { NextFetchEvent, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+  throw new Error(
+    "Please define the NEXT_PUBLIC_SUPABASE_URL environment variable inside .env.local"
+  );
+}
+
+if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error(
+    "Please define the NEXT_PUBLIC_SUPABASE_ANON_KEY environment variable inside .env.local"
+  );
+}
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  let authResult = await getUser(req);
+  if (authResult.error) {
+    console.log(
+      "Authorization error, redirecting to login page",
+      authResult.error
+    );
+    return NextResponse.redirect(`/login`);
+  } else if (!authResult.user) {
+    console.log("No auth user, redirecting");
+    return NextResponse.redirect(`/login`);
+  } else {
+    return NextResponse.next();
+  }
+}
+
+async function getUser(req: NextRequest) {
+  let token = req.cookies["sb:token"];
+  if (!token) {
+    return {
+      user: null,
+      data: null,
+      error: "There is no supabase token in request cookies",
+    };
+  }
+
+  let result = await (
+    await fetch(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/v1/user`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        APIKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
+      },
+    })
+  ).json();
+
+  if (
+    (
+      await fetch(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/v1/user`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          APIKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
+        },
+      })
+    ).status != 200
+  ) {
+    return {
+      user: null,
+      data: null,
+      error: `Supabase auth returned ${
+        (
+          await fetch(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/v1/user`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              APIKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
+            },
+          })
+        ).status
+      }. See logs for details`,
+    };
+  } else if (result.aud === "authenticated") {
+    return {
+      user: result,
+      data: result,
+      error: null,
+    };
+  }
+}
