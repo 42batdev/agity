0ef6083c625285f1752c7551b943acfdd862459c
Hendrik Schuster <h.schuster@deviantdev.com>
2022-01-16 21:36:56 +0100

    Make the init app code more resilent for missing profiles

--- apps/app/src/components/dashboard/initAppDashboardProps.ts
+++ apps/app/src/components/dashboard/initAppDashboardProps.ts
@@ -7,14 +7,14 @@ export interface AgityAppServerSideProps {
 }
 
 export const initAppDashboardProps = async (
-  context
+    context
 ): Promise<GetServerSidePropsResult<AgityAppServerSideProps>> => {
   const authResult = await supabase.auth.api.getUserByCookie(context.req);
 
   if (!authResult || !authResult.user || authResult.error) {
     console.log(
-      "Authorization error or no auth user redirecting to login page",
-      authResult.error
+        "Authorization error or no auth user redirecting to login page",
+        authResult.error
     );
     return {
       redirect: {
@@ -23,29 +23,40 @@ export const initAppDashboardProps = async (
       },
     };
   } else {
-    return {
-      props: {
-        profile: await createProfile(authResult.user),
-      },
-    };
+
+    const profile = await createProfile(authResult.user)
+    if (profile) {
+      return {
+        props: {
+          profile
+        },
+      };
+    } else {
+      return {
+        redirect: {
+          destination: `/404`,
+          permanent: false,
+        },
+      };
+    }
   }
 };
 
-async function createProfile(user: User) {
+async function createProfile(user: User): Promise<Profile | null> {
   const profileResult = await supabase
-    .from("profiles")
-    .select("id, username, displayname, avatar_url")
-    .match({ id: user.id });
+      .from("profiles")
+      .select("id, username, displayname, avatar_url")
+      .match({id: user.id});
 
-  supabase.storage.updateBucket("avatars", { public: true }).then(console.log);
+  supabase.storage.updateBucket("avatars", {public: true}).then(console.log);
 
   const profile: Profile = {
     username: profileResult.data[0].username,
     displayName: profileResult.data[0].displayname,
     avatar: {
       url: supabase.storage
-        .from("avatars")
-        .getPublicUrl(profileResult.data[0].avatar_url).publicURL,
+          .from("avatars")
+          .getPublicUrl(profileResult.data[0].avatar_url).publicURL,
       filename: profileResult.data[0].avatar_url,
     },
   };
