feecfc710a6a431d17f9d6e35bd21faafb09b9de
Hendrik Schuster <h.schuster@deviantdev.com>
2022-03-04 23:32:16 +0100

    Show a text why the Owner and own user could not be edited

--- apps/app/src/components/team/members/MemberList.tsx
+++ apps/app/src/components/team/members/MemberList.tsx
@@ -1,12 +1,10 @@
 import { canEditTeam } from "../../../functions/permissions";
-import { useUser } from "../../../supabase/AuthContext";
 import { CardGrid } from "../../common/card/CardGrid";
 import { useTeam } from "../hooks/useTeam";
 import { MemberCard } from "./card/MemberCard";
 import React from "react";
 
 export const MemberList = () => {
-  const user = useUser();
   const { loading, data } = useTeam();
 
   return (
@@ -20,10 +18,7 @@ export const MemberList = () => {
                 key={member.profile.id}
                 team={data.getTeam}
                 member={member}
-                disabled={
-                  !canEditTeam(data.getTeam.myPermissions) ||
-                  member.profile.id === user.id
-                }
+                disabled={!canEditTeam(data.getTeam.myPermissions)}
               />
             );
           }
